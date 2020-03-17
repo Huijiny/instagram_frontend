ef08969aea357dc4194539cdb34496605b75ec5a
heejin ryu <jiin20803@gmail.com>
2020-03-18 00:25:47 +0900

    ui components

---
+++ src/Components/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {gql} from "apollo-boost";
+import styled, { ThemeProvider } from "styled-components";
+import GlobalStyles from "../Styles/GlobalStyles"
+import Theme from "../Styles/Theme"
+import Router from './Router';
+import { useQuery } from "react-apollo-hooks" 
+import Footer from "./Footer"
+
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 935px;
+  width: 100%;
+`;
+
+
+export default () => {
+
+  const {data : {isLoggedIn}} = useQuery(QUERY);
+ 
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <Wrapper>
+        <GlobalStyles/>
+        <Router isLoggedIn={isLoggedIn}/>
+        <Footer></Footer>
+      </Wrapper>
+    </ThemeProvider>
+  )
+}
