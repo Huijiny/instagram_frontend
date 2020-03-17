37b64816b379905e09a5dc7bab9f4a3477e8e7f6
heejin ryu <jiin20803@gmail.com>
2020-03-18 00:24:09 +0900

    set up
    setup insta clone frontend

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {ApolloProvider} from "react-apollo-hooks"
+import Client from './Apollo/Client'
+import App from './Components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ApolloProvider client={Client}><App /></ApolloProvider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
