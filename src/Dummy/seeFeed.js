3273347a110e5a811b6ad9597506e1b7cad8a8d4
heejin ryu <jiin20803@gmail.com>
2020-03-25 00:10:00 +0900

    post component
    post컴포넌트 제작 후, proptypes까지 지정 및 dummy data 이용해 값 전달 완료

---
+++ src/Dummy/seeFeed.js
@@ -0,0 +1,36 @@
+
+const data = {
+    seeFeed:[
+        {
+            id:"cjt40j5tiffo0b7652vf9taj",
+            location: "Seoul",
+            caption: "asdf",
+            user: {
+                id : "12334556457567",
+                avatar: null,
+                username: "heejini"
+            },
+            files: [
+                {
+                    id: "123123",
+                    url: "http:// serranoarevalo.com/wp-content/uploads/2017/04/NicoSerranoArevalo0552-1920x1272.jpg"
+                },
+                {
+                    id:"!231325456567",
+                    url:"http:// serranoarevalo.com/wp-content/uploads/2017/04/NicoSerranoArevalo0552-1920x1272.jpg"
+                },
+                {
+                    id:"!231425456567",
+                    url:"http:// serranoarevalo.com/wp-content/uploads/2017/04/NicoSerranoArevalo0552-1920x1272.jpg"
+                }
+            ],
+            likeCount: 2,
+            isLiked: false,
+            comments:[],
+            createdAt:" 2019-03-11T08:11:09.127Z"
+        }
+    ]
+
+};
+
+export default data;
