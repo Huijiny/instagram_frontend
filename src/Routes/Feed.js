d7257630967cdd3ee09bf9935ef66346f296ec8b
heejin ryu <jiin20803@gmail.com>
2020-03-25 00:09:18 +0900

    Feed
    feed 에 post 값 가져와서 뿌려주기 전, footer와 loader 제작

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,1 +1,71 @@
-export default () => "Feed";
+import React from 'react'
+import { gql } from 'apollo-boost'
+import styled from 'styled-components'
+import { useQuery } from 'react-apollo-hooks';
+import Loader from '../Components/Loader';
+import data from '../Dummy/seeFeed';
+import Post from '../Components/Post'
+
+const FEED_QUERY = gql`
+    {
+        seeFeed{
+            id
+            location
+            caption
+            user {
+                id
+                avatar
+                username
+            }
+            files {
+                id
+                url
+            }
+            likeCount
+            isLiked
+            comments {
+                id
+                text
+                user {
+                    id
+                    username
+                }
+            }
+            createdAt
+        }
+    }
+`;
+
+const Wrapper = styled.div` 
+    display : flex;
+    flex-direction : column;
+    align-items : center;
+    min-height : 80vh;
+`
+
+
+export default () => {
+    //const {data, loading} = useQuery(FEED_QUERY);
+    console.log(data);
+    let loading = false;
+return (
+    <Wrapper>
+        { loading && <Loader/> }
+        {!loading &&
+        data &&
+        data.seeFeed && 
+        data.seeFeed.map( post => (
+            <Post
+            key = {post.id}
+            id = {post.id}
+            user = {post.user}
+            files = {post.files}
+            likeCount = {post.likeCount}
+            isLiked = {post.isLiked}
+            comments = {post.comments}
+            createdAt = {post.createdAt}
+            />
+        ))}
+    </Wrapper>
+    );
+};
