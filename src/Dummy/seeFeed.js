90a1947a7bc4cb2d4400fe4bc74da1754608814f
heejin ryu <jiin20803@gmail.com>
2020-03-26 18:03:42 +0900

    Header, Icons 변경 및 이미지 주소 변경

--- src/Dummy/seeFeed.js
+++ src/Dummy/seeFeed.js
@@ -13,19 +13,19 @@ const data = {
             files: [
                 {
                     id: "123123",
-                    url: "http:// serranoarevalo.com/wp-content/uploads/2017/04/NicoSerranoArevalo0552-1920x1272.jpg"
+                    url: "https://mblogthumb-phinf.pstatic.net/20150812_87/toreore9292_1439356155923ewN3z_PNG/%B6%C7%B7%A1%BF%C0%B7%A1_%C4%A1%C5%B2_%B0%ED%C8%AD%C1%FA_%C0%A7%B2%C3%BB%E7.2.png?type=w2"
                 },
                 {
                     id:"!231325456567",
-                    url:"http:// serranoarevalo.com/wp-content/uploads/2017/04/NicoSerranoArevalo0552-1920x1272.jpg"
+                    url:"https://mblogthumb-phinf.pstatic.net/20150812_290/toreore9292_1439356454818iMzTs_PNG/%B6%C7%B7%A1%BF%C0%B7%A1_%C4%A1%C5%B2_%B0%ED%C8%AD%C1%FA_%C0%A7%B2%C3%BB%E7.3.png?type=w2"
                 },
                 {
                     id:"!231425456567",
-                    url:"http:// serranoarevalo.com/wp-content/uploads/2017/04/NicoSerranoArevalo0552-1920x1272.jpg"
+                    url:"https://mblogthumb-phinf.pstatic.net/20150812_53/toreore9292_1439356965216v7RjB_PNG/%B6%C7%B7%A1%BF%C0%B7%A1_%C4%A1%C5%B2_%B0%ED%C8%AD%C1%FA_%C0%A7%B2%C3%BB%E7.5.png?type=w2"
                 }
             ],
             likeCount: 2,
-            isLiked: false,
+            isLiked: true,
             comments:[],
             createdAt:" 2019-03-11T08:11:09.127Z"
         }
